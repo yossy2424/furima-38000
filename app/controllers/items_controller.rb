1081c9b6aa471c9f6cc15efe7b415a4f37ae7504
yossy2424 <yossy4415007@gmail.com>
2022-07-13 18:13:46 +0900

    商品一般表示機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @item = Item.all    #.order("created_at DESC")
+    @item = Item.includes(:user).order("created_at DESC")
   end
 
   def new
