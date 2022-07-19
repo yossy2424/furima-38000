1b303b1b2ef7ff6d2abe377a710edd2912815c30
yossy2424 <yossy4415007@gmail.com>
2022-07-19 10:35:20 +0900

    商品削除機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update,:show,]
+  before_action :set_item, only: [:edit, :update,:show,:destroy]
   before_action :authenticate_user!, except: [:index,:show]
   def index
     @item = Item.includes(:user).order("created_at DESC")
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
   private
