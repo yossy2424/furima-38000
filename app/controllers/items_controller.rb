462b573172ee0d7c3a48f4728acb6f605a51f3d8
yossy2424 <yossy4415007@gmail.com>
2022-07-19 14:36:20 +0900

    商品削除機能２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,9 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  if current_user.id == @item.user_id
     if @item.destroy
       redirect_to root_path
     end
+  end
   end
   private
 
