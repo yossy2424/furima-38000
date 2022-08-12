7dadfe85dd3be5fd6b47ae173366641b8f3b36f5
yossy2424 <yossy4415007@gmail.com>
2022-08-12 15:37:56 +0900

    コメント機能

---
+++ app/javascript/channels/comment_channel.js
@@ -0,0 +1,31 @@
+import consumer from "./consumer"
+
+if(location.pathname.match(/\/items\/\d/)){
+
+  consumer.subscriptions.create({
+    channel: "CommentChannel",
+    item_id: location.pathname.match(/\d+/)[0]
+  }, {
+
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    const html = `
+    <div class="comment">
+      <p class="user-info">${data.user.nickname}: </p>
+      <p>${data.comment.text}</p>
+    </div>`
+  const comments = document.getElementById("comments")
+  comments.insertAdjacentHTML('beforeend', html)
+  const commentForm = document.getElementById("comment-form")
+  commentForm.reset();
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
+}
